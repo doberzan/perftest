3dfe964f22e98d8f9c7842f963b84bd36dea130c
Declan Oberzan <declan.oberzan@sencha.com>
2017-06-05 15:59:06 -0500

    Removed stuff v2

--- WebServer.js
+++ WebServer.js
@@ -1,36 +1,76 @@
 const serveStatic = require('serve-static');
 const fs = require("fs");
+var qs = require('querystring');
 const http = require('http');
 const finalhandler = require('finalhandler');
 const payload = '<script id="script1" type="text/javascript" src="hack.js"></script></head>'
 var port = 80;
 var hostname = '127.0.0.1';
+var reset = "\x1b[0m", green = "\x1b[32m", red = "\x1b[31m", blue = "\x1b[34m", black = "\x1b[1m" + "\x1b[30m";
+var plus = (black + "[" + green + "+" + black +"]");
 
-function setup(){
-  var serve = serveStatic('/Users/declan/Sencha/QuickStart/', {
-    'index': ['index.html', 'index.htm']
-  })
+function getData(req, res){
+    console.log(black + "\n============================================");
+    console.log(blue + "Received post at url: ", green + req.url);
+    console.log(blue + "Agent Info: ", green, req.headers["user-agent"]);
+    console.log(reset);
+    if (req.url === "/index.html" || req.url === "/") {
+        var requestBody = '';
+        req.on('data', function(data) {
+            requestBody += data;
+        });
 
-  var server = http.createServer(function onRequest (req, res) {
-
-    if(req.url == '/' || req.url == '/index.html'){
-      var data = fs.readFileSync('/Users/declan/Sencha/QuickStart/index.html','utf-8');
-      var data2 = data.split('</head>');
-      var finaldata = data2[0] + payload + data2[1];
-      fs.writeFileSync('/Users/declan/Sencha/QuickStart/foo.html', finaldata);
-      req.url = '/foo.html';
+        req.on('end', function() {
+            var formData = JSON.parse(requestBody);
+            console.log(blue + 'Results after' + green, formData.length + blue + ' seconds.')
+            console.log(green + "FPS: " + blue, requestBody);
+            console.log(black + "============================================\n");
+            console.log(reset);
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({
+                success: true
+            }));
+        });
+    } else {
+        console.log(red+"Sending 404");
+        console.log(reset);
+        res.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/html'});
+        res.end('Error');
     }
-
-    serve(req,res, finalhandler(req, res));
-  })
-
-  server.listen(port, hostname, function(){return console.log('Started server on ', hostname, ' at port ', port)});
 }
 
-if(process.argv.length < 4) {
-    console.log('[!] Not enough arguments! Syntax: node WebServer.js <hostname> <port>');
-  }else {
+
+
+function setup(){
+    var serve = serveStatic(__dirname, {
+        'index': ['index.html', 'index.htm']
+    })
+
+    var server = http.createServer(function onRequest (req, res) {
+
+        if(req.method === "POST") {
+            getData(req, res);
+            return;
+        } 
+        if (req.url == '/' || req.url == '/index.html') {
+            var data = fs.readFileSync(__dirname + 'index.html','utf-8');
+            var data2 = data.split('</head>');
+            var finaldata = data2[0] + payload + data2[1];
+            fs.writeFileSync('/Users/declan/Sencha/perftest/foo.html', finaldata);
+            req.url = '/foo.html';
+        }
+        serve(req,res, finalhandler(req, res));
+    });
+    
+    server.listen(port, hostname, function(){return console.log(plus + blue + ' Started test server on' + green, hostname, blue+ 'at port' + green, port + reset)});
+  }
+
+if (process.argv.length < 4) {
+    console.log(red + '[!] Not enough arguments! Syntax: node WebServer.js <hostname> <port>');
+    console.log(reset);
+} else {
     hostname = process.argv[2];
     port = process.argv[3];
     setup();
-  }
+    console.log(reset);
+}
--- hack.js
+++ hack.js
@@ -1,24 +1,72 @@
+var intervalId;
+var timebase;
+var tickCount = 0; 
+var FPS = [];
+
 function doTests(){
     var cmp = Ext.getCmp('thegrid');
     if(!cmp){
         setTimeout(doTests, 50);
         return
     }
-    var num = 1;
-    console.log('hello')
-    setInterval(function(){
-        console.log(cmp.getScrollable().getMinPosition());
-        console.log(cmp.getScrollable().getPosition());
-        if((cmp.getScrollable().getMaxPosition()-1) == cmp.getScrollable().getPosition()){
-            num = -1;
-            console.log(num);
-        }else if((cmp.getScrollable().getMinPosition()+1) == cmp.getScrollable().getPosition()){
-            num = 1;
-            console.log(num);
-        }
-            cmp.getScrollable().scrollBy(null, num * 7);
-    }, 1);
+    requestAnimationFrame(tick);
+    intervalId = setInterval(scrollDown,1);
 }
+
+function scrollDown(){
+    var cmp = Ext.getCmp('thegrid');
+    var num = 1;
+    var scroller = cmp.getScrollable();
+    if(scroller.getMaxPosition().y == scroller.getPosition().y){
+        clearInterval(intervalId);
+        intervalId = 0;
+        console.log(FPS);
+        sendData(FPS);
+    }
+    scroller.scrollBy(null, 7);
+}
+
+function scrollUp(){
+    var cmp = Ext.getCmp('thegrid');
+    var num = 1;
+    var scroller = cmp.getScrollable();
+    if(0 == scroller.getPosition().y){
+        clearInterval(intervalId);
+    }
+    scroller.scrollBy(null, -7);
+}
+
+function tick(t){
+    if(!timebase){
+        timebase = t;
+    }else{
+        tickCount ++;
+        var deltaT = (t-timebase) / 1000;
+        if(deltaT > 1){
+            var fps = Math.round(tickCount / deltaT);
+            FPS.push(fps);
+            tickCount = 0;
+            timebase = t;
+        }
+    }
+    if(intervalId){
+        requestAnimationFrame(tick)
+    };
+}
+
+function sendData(data){
+    fetch(window.location.href, {
+        method: 'post',
+        body: JSON.stringify(data)
+    }).then(function(response){
+        response.json().then(function(j){
+            window.location.href = j.redirect;
+        });
+    }, function(err){   
+        console.log(err)
+    });
+}
+
 window.onload = function(){
     if(Ext.onReady){
         Ext.onReady(doTests);
@@ -28,8 +76,3 @@ window.onload = function(){
         }
     }
 };
-
-
-//var cmp = Ext.getCmp('thegrid');
-//cmp.getScrollable().scrollBy(null, 5);
-//console.log(setInterval(function(){cmp.getScrollable().scrollBy(null, 5)}, 1));
