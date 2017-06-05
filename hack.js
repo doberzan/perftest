6a3fb381d0ecb80451b72818949f2e37c5a41132
Declan Oberzan <declan.oberzan@sencha.com>
2017-06-05 11:08:18 -0500

    Created bounce scroll

--- hack.js
+++ hack.js
@@ -4,8 +4,19 @@ function doTests(){
         setTimeout(doTests, 50);
         return
     }
+    var num = 1;
+    console.log('hello')
     setInterval(function(){
-        cmp.getScrollable().scrollBy(null, 5);
+        console.log(cmp.getScrollable().getMinPosition());
+        console.log(cmp.getScrollable().getPosition());
+        if((cmp.getScrollable().getMaxPosition()-1) == cmp.getScrollable().getPosition()){
+            num = -1;
+            console.log(num);
+        }else if((cmp.getScrollable().getMinPosition()+1) == cmp.getScrollable().getPosition()){
+            num = 1;
+            console.log(num);
+        }
+            cmp.getScrollable().scrollBy(null, num * 7);
     }, 1);
 }
 window.onload = function(){
