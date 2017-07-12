ea2837157149b5891494813f80f4a6e5cff92e55
Declan Oberzan <declan.oberzan@sencha.com>
2017-07-12 17:13:05 -0500

    wip

--- cli/cli.js
+++ cli/cli.js
@@ -24,7 +24,6 @@ function fetch(server, path, data){
             
             res.on('end', function () {
                 try{
-                    console.log(responseBody);
                     let j = JSON.parse(responseBody);
                     resolve(j);
                 }catch(e){
@@ -83,44 +82,6 @@ function runTestSequence(agent, tests, server, testPage){
 
 }
 
-function runTest(results, agent, test, server){
-    //generate id for test agent uuid  (goto /test/?id='uuid')
-    //tell parked agent to go to place to do a test
-    let agentUuid = uuidv4();
-    console.log('Sending agent \'' + agent + '\' with uuid: \'' +  agentUuid + '\' to run test: ' + '\'' + test + '\'');
-    return fetch(server, '/~api/cmd/', {
-        agent:agent,
-        cmd:{
-            type:'redirect',
-            data:testPages[test] + '?id=' + agentUuid
-        }
-    }).then(function(data){
-        //connect back to hack.js
-        //tell test agent to run test
-        return fetch(server, '/~api/cmd/',{
-            agent:agentUuid,
-            cmd:{
-                type:test,
-                data:test
-            }
-        }
-        ).then(function(data){
-            //tell agent to redirect to park
-            results[test] = agent;
-            return fetch(server, '/~api/cmd/',{
-                agent:agentUuid,
-                cmd:{
-                    type:'redirect',
-                    data:'/park/?id=' + agent
-                }
-            }).then(function(){
-                console.log('done')
-            });
-        })
-    })
-    
-}
-
 function runTests(agents, tests, server, testPage){
     agents = agents.split(',');
     tests = tests.split(',');
@@ -150,7 +111,6 @@ class sendCMD extends Command {
                     data:params.test
                 }
             }).then(function(data){ 
-                console.log(data)
             });
         }else{
             let logfile = fs.createWriteStream('RESULTS.md', {
@@ -158,7 +118,6 @@ class sendCMD extends Command {
             })
             return runTests(params.agent, params.test, params.server, params.testpage).then(function(results){
                 //logfile.write('## \t' + h + ':' + minutes + ':' + sec + ' ' + m + '/' +  d + '/' + y + '\n');
-                console.log(results);
                 for(let agent in results) {
                     logfile.write('# ' + agent.toUpperCase() + '\n');
                     console.log('# ' + agent.toUpperCase());
