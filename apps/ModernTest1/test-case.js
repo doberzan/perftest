fd84e4e1f3912e4ea3e5144bdb8f6c0a4fca6811
Declan Oberzan <declan.oberzan@sencha.com>
2017-08-09 17:50:46 -0500

    wip

--- apps/ModernTest1/test-case.js
+++ apps/ModernTest1/test-case.js
@@ -53,7 +53,7 @@ function scrollDown(timerid){
         return calculate(('Scrolled '+ scroller.getMaxPosition().y +' pixels down in '+ sec +' mili-seconds.'));
     }
     //let rand2 = Math.floor(Math.random() * 500);
-    scroller.scrollBy(null, 10);
+    scroller.scrollBy(null, 100);
 }
 
 //Calculates data
@@ -107,7 +107,7 @@ function scrollUp(timerid){
         console.log(sec);
         return calculate(('Scrolled '+ scroller.getMaxPosition().y +' pixels up in '+ sec +' mili-seconds.'));
     }
-    scroller.scrollBy(null, -10);
+    scroller.scrollBy(null, -100);
 }
 
 function loadTest(timerid){
