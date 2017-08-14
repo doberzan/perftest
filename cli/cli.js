b5dd21763938bffe75311835810fee4296461f6f
Declan Oberzan <declan.oberzan@sencha.com>
2017-08-14 18:13:40 -0500

    wip

--- cli/cli.js
+++ cli/cli.js
@@ -177,8 +177,8 @@ function compareResultToHistory(results){
                     hrmean = round(hrmean,1);
                     fpsmean = round(fpsmean,1);
                     hrstd = round(hrstd, 1)
-
-                    if(fpsmean >= hrmean - hrstd * tolerance){
+                    hrstd *= tolerance;
+                    if(fpsmean >= hrmean - hrstd){
                         resultfile.write(`- ${test} - OK (${fpsmean} vs ${hrmean} +/- ${hrstd})\n`);
                         console.log(`OK: (${fpsmean} vs ${hrmean} +/- ${hrstd})`);
                     }else{
@@ -194,7 +194,7 @@ function compareResultToHistory(results){
                         hrmean = round(hrmean,2);
                         num = round(num,2);
                         hrstd = round(hrstd, 4)
-                        if(num <= hrmean + hrstd * tolerance){
+                        if(num <= hrmean + hrstd){
                             resultfile.write(`- ${result} - OK (${num}ms vs ${hrmean}ms +/- ${hrstd}ms)\n`);
                             console.log(`OK: (${num}ms vs ${hrmean}ms +/- ${hrstd}ms)`);
                         }else{
